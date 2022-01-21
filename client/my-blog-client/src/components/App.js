e46080da87c92690ef5a43efc29946edfbda3fbd
Kris Norful <krisnorful@gmail.com>
2022-01-21 11:57:03 -0600

    update dependencies in pom to satisfy CORS

--- client/my-blog-client/src/components/App.js
+++ client/my-blog-client/src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { getPosts } from '../proxies/proxies';
 
 export const App = () => {
+    let posts = getPosts();
     return <h1>Hello</h1>
 }
--- client/my-blog-client/src/proxies/proxies.js
+++ client/my-blog-client/src/proxies/proxies.js
@@ -1,2 +1,8 @@
 const axios = require('axios');
 
+export const getPosts = () => {
+    let posts = axios.get('http://localhost:8080/blog')
+        .then(res => res.data);
+    console.log("posts => ", posts);
+    return posts;
+}
